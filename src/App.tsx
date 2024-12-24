c0adeede0e104fd55e49a395cdc9d4d19bf095c6
joffutt-dev <joeoffutt4@gmail.com>
2024-12-24 00:11:48 -0500

    initial tailwind set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Header } from "./components/Header";
 import { SullyRouter } from "./containers/SullyRouter";
-import "./styles/mainStyles.css";
 
 export function App() {
   return (
-    <>
+    <div className="dark">
       <Header />
       <SullyRouter />
-    </>
+    </div>
   );
 }
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,8 +1,12 @@
 export function Home() {
   return (
     <div>
-      This is the home page
-      <div>Carousel</div>
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+          Test Dark BG
+        </h3>
+        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">here</p>
+      </div>
     </div>
   );
 }
